0f3b0f402e4145b7368d3332ffc96870cb6f42e6
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-19 18:06:23 -0600

    use headers to display more info

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,9 @@
 // refrence to the issue container
 var issueContainerEl = document.querySelector("#issues-container");
 
+// limit warning container
+var limitWarningEl = document.querySelector("#limit-warning");
+
 // get repo issues takes repo name as a parameter
 var getRepoIssues = function(repo) {
     // API endpoint and variable to define the query
@@ -12,11 +15,14 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to the DOM function
                 displayIssues(data);
-            });
+
+                // check if api has paginated issues
+                displayWarning(repo)
+            })
         } else {
             alert("There was a problem with your request!");
         }
-    });
+    })
 }
 
 // Turning github issue data into DOM element
@@ -58,6 +64,19 @@ var displayIssues = function(issues) {
         // append elements to page
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com"
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container 
+    limitWarningEl.appendChild(linkEl)
 }
 
 getRepoIssues("facebook/react");
